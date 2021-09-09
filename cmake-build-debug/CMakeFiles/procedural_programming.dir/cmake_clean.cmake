39b1691206525b552e05fbc70ed15d38de4a52dc
Mark Terris <max_pon_04@mail.ru>
2021-09-09 23:44:48 +0300

    test 0.1.1 clion setup

---
+++ cmake-build-debug/CMakeFiles/procedural_programming.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/procedural_programming.dir/test-progs/test0.cpp.obj"
+  "libprocedural_programming.dll.a"
+  "procedural_programming.exe"
+  "procedural_programming.exe.manifest"
+  "procedural_programming.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/procedural_programming.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
