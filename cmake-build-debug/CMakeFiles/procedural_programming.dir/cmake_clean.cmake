bac71f54fdc95189ee6bca53255890c45d8acb60
Mark Terris <max_pon_04@mail.ru>
2021-09-11 02:07:30 +0300

    версия 0.1.0 пофиксил все баги кроме одного ввод формата цифры + буквы просто обрезает все буквы нужно исправить чтобы это считалось некорректным выполненные задачи: 1 2 3

--- cmake-build-debug/CMakeFiles/procedural_programming.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/procedural_programming.dir/cmake_clean.cmake
@@ -1,5 +1,9 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/procedural_programming.dir/test-progs/test0.cpp.obj"
+  "CMakeFiles/procedural_programming.dir/test-progs/double_type_input.cpp.obj"
+  "CMakeFiles/procedural_programming.dir/test-progs/index.cpp.obj"
+  "CMakeFiles/procedural_programming.dir/test-progs/zad1.cpp.obj"
+  "CMakeFiles/procedural_programming.dir/test-progs/zad2.cpp.obj"
+  "CMakeFiles/procedural_programming.dir/test-progs/zad3.cpp.obj"
   "libprocedural_programming.dll.a"
   "procedural_programming.exe"
   "procedural_programming.exe.manifest"
