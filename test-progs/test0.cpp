6cd96768f03beedb4120fc59b5e23c105fccaf96
IKBOstudent <maxpon225@gmail.com>
2021-09-10 10:50:23 +0300

    test 0.2.1 first prog in class

--- test-progs/test0.cpp
+++ test-progs/test0.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include <windows.h>
 
+
 using namespace std;
+
+int foo1(){
+    int a, b;
+    cin >> a >> b;
+    return a+b;
+}
+
+int foo2(){
+}
 
 int main(){
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-
-    cout << "привет\n";
+    int summa = foo1();
+    cout << summa;
     return 0;
 }
