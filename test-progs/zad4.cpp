0b8befa7475d4b9df155fb56d8a351d299e5470e
Mark Terris <max_pon_04@mail.ru>
2021-09-11 14:20:29 +0300

    версия 0.2.0 все правки внесены выполненные задачи: дз 1

---
+++ test-progs/zad4.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include "zads.h"
+#include <cmath>
+
+using namespace std;
+
+void zad_yeshyo_uravneniye(){
+    cout << "--------------------------------\n";
+    cout << "начало задания 4\n\n";
+    cout << "эта программа выводит решение уранения вида: ax^2 + bx + c = 0\n";
+
+
+    // Уравнение вида: ax^2 + bx + c = 0
+    long double answer1, answer2, a, b, c;
+    bool vvod = true;
+
+    while (vvod){
+        cout << "[ввод коэффициента a]\n";
+        a = double_type_input();
+
+        cout << "[ввод коэффициента b]\n";
+        b = double_type_input();
+
+        cout << "[ввод коэффициента c]\n";
+        c = double_type_input();
+
+        cout << "Вы ввели a = " << a << "; b = " << b << "; c = " << c << endl;
+        cout << "чтобы продолжить введите [y]; чтобы повторить ввод введите [n]" << endl;
+        string y_n;
+        cin >> y_n;
+        while (y_n != "y" and y_n != "n") {
+            cout << "ввод некорректный!\n";
+            cout << "чтобы продолжить введите [y]; чтобы повторить ввод введите [n]" << endl;
+            cin >> y_n;
+        }
+        if (y_n == "y") {
+            vvod = false;
+        }
+    }
+
+    long double d = b*b - 4*a*c;
+
+    cout << "\nвывод задания 4:\n\n";
+
+    if (a == 0){
+        if (b != 0) {
+            answer1 = -c / b;
+            if (answer1 == 0)
+                answer1 = 0;
+
+            cout << "x = " << answer1 << endl;
+        } else {
+            cout << "x - любое число\n";
+        }
+
+    }
+    else if (d == 0) {
+        answer1 = -b / (2 * a);
+        if (answer1 == 0)
+            answer1 = 0;
+        cout << "x = " << answer1 << endl;
+
+    } else if (d > 0) {
+        answer1 = (-b + sqrt(d)) / (2 * a);
+        if (answer1 == 0)
+            answer1 = 0;
+        answer2 = (-b - sqrt(d)) / (2 * a);
+        if (answer2 == 0)
+            answer2 = 0;
+        cout << "x1 = " << answer1 << "x2 = " << answer2 << endl;
+
+    } else {
+        cout << "нет корней" << endl;
+    }
+
+    cout << "\nконец задания 4\n";
+    cout << "--------------------------------\n\n";
+}
---
+++ test-progs/zad5.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include "zads.h"
+
+using namespace std;
+
+string y_n_input(){
+    string y_n;
+
+    cout << "введите [y] если да; [n] если нет" << endl;
+    cin >> y_n;
+
+    while (y_n != "y" and y_n != "n") {
+        cout << "ввод некорректный!\n";
+        cout << "введите [y] если да; [n] если нет" << endl;
+        cin >> y_n;
+    }
+
+    return y_n;
+}
+
+void zad_lampa_so_shtorkoy(){
+    cout << "--------------------------------\n";
+    cout << "начало задания 5\n\n";
+    cout << "эта программа определяет светло ли в команате\n";
+
+    string answer;
+
+    // светло ли в комнате
+    cout << "лампа включена?\n";
+    answer = y_n_input();
+    if (answer == "y")
+        cout << "в комнате светло\n";
+    else {
+        cout << "шторы раздвинуты?\n";
+        answer = y_n_input();
+        if (answer == "n")
+            cout << "в комнате темно\n";
+        else {
+            cout << "на улице день?\n";
+            answer = y_n_input();
+            if (answer == "y")
+                cout << "в комнате светло\n";
+            else
+                cout << "в комнате темно\n";
+        }
+    }
+
+
+    cout << "\nконец задания 5\n";
+    cout << "--------------------------------\n\n";
+}
